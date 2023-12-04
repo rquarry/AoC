2653559648f7545dda814f9a8ad1ba3cad31eceb
Ryan Quarry <rquarry1@yahoo.com>
2023-12-03 21:09:48 -0500

    parse first and last digits to ascii

--- AOC_2023/1/aoc_2023-1.go
+++ AOC_2023/1/aoc_2023-1.go
@@ -11,6 +11,9 @@ func main() {
 
 	var cal_values = "1234567890"
 
+	// placeholder; won't compile otherwise
+	//var cal_valuesSum int
+
 	file, err := os.Open("input.txt")
 	if err != nil {
 		fmt.Println(err)
@@ -21,7 +24,22 @@ func main() {
 	for scanner.Scan() {
 		//fmt.Println(scanner.Text())
 		var line = scanner.Text()
-		fmt.Println(strings.IndexAny(line, cal_values), strings.LastIndexAny(line, cal_values))
+
+		// Print test that index functions work as expected
+		//fmt.Println(strings.IndexAny(line, cal_values), strings.LastIndexAny(line, cal_values))
+
+		// pull out first number from line; result is ascii number val
+		var first_AsciiDigit = line[strings.IndexAny(line, cal_values)]
+
+		// pull out last number from line; result is ascii number val
+		var last_AsciiDigit = line[strings.LastIndexAny(line, cal_values)]
+
+		// test print that it work; it does :-)
+		fmt.Println(first_AsciiDigit, last_AsciiDigit)
+
+		// Use Atio or ParseInt to convert to integers and sum
+		// https://gobyexample.com/number-parsing
+
 	}
 
 	if err := scanner.Err(); err != nil {
